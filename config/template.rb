246228d081b134c0123424629bcd825245dba4c6
stephane de la torre <s.dlt@hotmail.fr>
2018-10-12 17:19:53 +0200

    Add mailer config

--- config/template.rb
+++ config/template.rb
@@ -1,8 +1,12 @@
+def bullet_config
+  'config.after_initialize do
+    Bullet.enable        = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+  end'
+end
+
 copy_file 'config/sidekiq.yml'
 copy_file 'config/initializers/redis.rb'
-environment "config.after_initialize do\n
-  Bullet.enable        = true\n
-  Bullet.bullet_logger = true\n
-  Bullet.console       = true\n
-  Bullet.rails_logger  = true\n
-end\n", env: 'development'
+environment bullet_config, env: 'development'
