b4e71c0c73c7edcbf5977591b06fd64dbbffcff3
stephane de la torre <s.dlt@hotmail.fr>
2018-10-12 23:49:46 +0200

    Fix sidekiq routes indentation

--- config/template.rb
+++ config/template.rb
@@ -13,6 +13,17 @@ def mailer_config
   "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }"
 end
 
+def sidekiq_routes
+  <<~RUBY
+    require 'sidekiq/web'
+    require 'sidekiq-status/web'
+
+    authenticate :user, lambda { |u| u.admin } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+  RUBY
+end
+
 copy_file 'config/sidekiq.yml' if @sidekiq
 copy_file 'config/initializers/redis.rb' if @sidekiq
 environment mailer_config, env: 'development' if @devise
@@ -20,14 +31,5 @@ environment bullet_config, env: 'development'
 route "root to: 'pages#home'"
 
 if @sidekiq
-  insert_into_file 'config/routes.rb', after: /draw do\n/ do
-    <<~RUBY
-      require 'sidekiq/web'
-      require 'sidekiq-status/web'
-      
-      authenticate :user, lambda { |u| u.admin } do
-        mount Sidekiq::Web => '/sidekiq'
-      end
-    RUBY
-  end
+  insert_into_file 'config/routes.rb', sidekiq_routes, after: /draw do\n/
 end
